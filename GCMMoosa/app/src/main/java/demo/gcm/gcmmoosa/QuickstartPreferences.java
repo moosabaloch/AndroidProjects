2af08253280fe060d018f32890c88d8124a5ce5c
moosabaloch <moosa.bh@gmail.com>
2015-12-29 16:16:51 +0500

    GCM Project Added

---
+++ GCMMoosa/app/src/main/java/demo/gcm/gcmmoosa/QuickstartPreferences.java
@@ -0,0 +1,20 @@
+package demo.gcm.gcmmoosa;
+
+/**
+ * Created by Moosa on 12/29/2015.
+ * Dear Maintainer
+ * When i wrote this code Only i and God knew What it was.
+ * Now only God Knows..!
+ * So if you are done trying to optimize this routine and Failed
+ * Please increment the following counter as the warning to the next Guy.
+ * TOTAL_HOURS_WASTED_HERE=1
+ */
+
+public class QuickstartPreferences {
+
+    public static final String SENT_TOKEN_TO_SERVER = "sentTokenToServer";
+    public static final String REGISTRATION_COMPLETE = "registrationComplete";
+    public static final String SENDER_ID="756584497396";
+    public static final String SERVER_KEY_FOR_THIS_PROJECT="AIzaSyBWgmWxe3Vy9Hhsj6QAQhgtcvTNhqPDjG4";
+
+}
